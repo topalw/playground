7d207b6a100f2fd44772fb2f8c170421ef29966a
topalw <topalei@gmail.com>
2022-04-01 11:37:39 +0200

    added some function stats

--- k_groups_of_equal_sum.r
+++ k_groups_of_equal_sum.r
@@ -1,8 +1,11 @@
 setwd('C:/Users/topalw/Desktop/')
 dat <- read.csv('contigs.lengths')
+
+
 # the logic behind it 
 plot(cumsum(dat$Length))
 abline(h=(sum(dat$Length)/10*1:length(dat$Length)))
+
 
 ### FUNCTION ###
 divider <- function(dist, chunks=10,reps=100){
@@ -12,6 +15,7 @@ divider <- function(dist, chunks=10,reps=100){
   # step and a minimize the variance of sums of all groups 
   # However there are still errors ex an equidistant point will fail or there might be 
   # more optimal ways of dividing but for rough estimation this will do.
+  # also it is quite slow in reps > 1000 
   ##########################################
   # vector for sum of square deviations from target value
   ssr <- rep(0,reps)
@@ -24,7 +28,7 @@ divider <- function(dist, chunks=10,reps=100){
   # make cum addition
   cum.add <- cumsum(dist)
   # define targets and make result vector
-  targets <- sum(dist)/10 * seq(1,chunks)
+  targets <- sum(dist)/chunks * seq(1,chunks)
   result <- rep(0,length(dist))
   # loop through targets and assign groups
   last <- 0
@@ -46,15 +50,43 @@ divider <- function(dist, chunks=10,reps=100){
   # choose best ssr 
   best <- which(ssr == min(ssr))[1] # might be > 1 
   # return best order 
-  #return(ssr)
-  return(group.mat[,best])
+  return(ssr)
+  #return(group.mat[,best])
 }
-# for optimization
-#ssr <- divider(dat$Length,10,1000)
+
+
+### Checking the function ###  ran with return(ssr)
 #plot(ssr,ylab='variance between groups',xlab='replicate of random order')
 #hist(ssr,xlab='variance between groups / replicate')
+
+### What variances do different # of groups give? ###
+#min.var <- rep(0,9)
+#for(k in 2:10){
+#min.var[k-1] <- min(divider(dat$Length,k,1000))[1]
+#}
+#plot(y=min.var, x=seq(2,10)) # obviously larger and fewer groups have smaller variance
+
+### does the function find the smallest variance split consistently? ###
+#min.var <- rep(0,100)
+#for(k in 1:100){
+#  min.var[k] <- min(divider(dat$Length,10,1000))[1]
+#}
+#plot(y=min.var, x=seq(1,100))
+#hist(min.var) # range 1-5e+13 for 100 x 1000 reps
+
+### does the function depend a lot on the number of replicates? ###
+# min.var <- rep(0,length(seq(100,10000,by=100)))
+# for(k in seq(100,10000,by=100)){
+#   min.var[k/100] <- min(divider(dat$Length,10,k))[1]
+# }
+# plot(y=min.var, x=seq(100,10000,by=100))
+# abline(lm(min.var~seq(100,10000,by=100)))
+# summary(lm(min.var~seq(100,10000,by=100))) # -1.295e+09  2.284e+08  -5.671 1.43e-07 ***
+
+
+### Running the function ###  ran with return(group.mat[,best])
 dat$groups <- divider(dat$Length,10,1000)
-sum(abs(aggregate(dat$Length, by=list(dat$groups), FUN=sum)[,2] - sum(dat$Length)/10))
+# plot
 plot(aggregate(dat$Length, by=list(dat$groups), FUN=sum)[,2])
 abline(h=sum(dat$Length)/10)
-write.csv(dat,'conting_lengths_grouped.csv',quote=F, row.names = F)
+#write.csv(dat,'conting_lengths_grouped.csv',quote=F, row.names = F)
